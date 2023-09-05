c3ed2555175ea70f8091ca9b10c7ba8a4189e669
VhrandomUV <brandon.villalobos@alumnos.uv.cl>
2023-09-05 20:07:49 -0300

    1er commit

---
+++ main.c
@@ -0,0 +1,48 @@
+#include <stdio.h>
+
+
+
+void adentro(){
+    printf("Adentro");
+}
+
+void afuera(){
+    printf("Afuera");
+}
+
+
+
+void menu(){
+    int opcion;
+    printf("1.- Adentro\n");
+    printf("2.- Afuera\n");
+    printf("3.- Salir\n");
+    scanf("%d", &opcion);
+
+
+    switch (opcion)
+    {
+    case 1:
+        adentro();
+        break;
+    case 2:
+        afuera();
+        break;
+    case 3:
+        break;
+    
+    default:
+        printf("Opcion no valida");
+        break;
+    }
+
+}
+
+
+void main(){
+    printf("Reservas.... \n");
+
+    menu();
+
+}
+
