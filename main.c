624cf90adf1ff35d340c4464c4492f3522511366
VhrandomUV <brandon.villalobos@alumnos.uv.cl>
2023-11-03 21:20:45 -0300

    refactor 1

--- main.c
+++ main.c
@@ -4,57 +4,39 @@
 #define P_M4 2000
 #define P_M6 2500
 
-
+int precio = 0;
 
 struct Mesas
 {
     int cap;
-    char ubi[20]; // adentro ; afuera
     int prec;
-    char disp[20]; // libre ; ocupado
-
+    char *disp; // libre ; ocupado
+    char *ubi; // adentro ; afuera
 };
+struct Mesas mesa[2][3];
 
 
 
-
-
-/*estas funciones (adentro y afuera ) se ocuparan de realizar la logica correspondeinte para cada caso*/
-void adentro();
-
-void afuera();
-/*crea la reserva de la mesa */
-void reserva();
-
-/* comprueba si la mesa esta disponible*/
-void disponibilidad();
-
-/*menu de opciones*/
 void menu();
 
+int num_personas();
+
+void generador();
+
+void reserva();
+
+void continuar();
 
 
 void main(){
 printf("Reservas.... \n");
+    generador();
 
     menu();
-
+    printf("$%d\n", precio);
 }
 
-void adentro(){
-    printf("Adentro");
-}
 
-void afuera(){
-    printf("Afuera");
-}
-
-void reserva(){
-
-}
-void disponibilidad(){
-
-}
 
 void menu(){
     int opcion;
@@ -68,10 +50,12 @@ void menu(){
     switch (opcion)
     {
     case 1:
-        adentro();
+        
+        reserva(0);
+    
         break;
     case 2:
-        afuera();
+        reserva(1);
         break;
     case 3:
         break;
@@ -81,4 +65,101 @@ void menu(){
         menu();
         }
 
+}
+
+int num_personas(){
+    int n_personas;
+    printf("ingrese cuntas personas comeran en la mesa (de 1 a 6 perosnas): ");
+    scanf("%d", &n_personas);
+
+    n_personas = n_personas + n_personas%2;
+    if (n_personas >= 2 && n_personas <= 6){
+        return(n_personas);
+    }
+    
+    else
+        printf("numero no valido\n");
+        num_personas();
+        
+    
+}
+
+
+
+void generador(){
+    int i = 0;
+    int n = 2;
+
+    int prices[3] = {P_M2, P_M4, P_M6};
+
+    
+    for(i = 0; i < 3; i++){
+        mesa[0][i].ubi = "adentro";
+        mesa[0][i].cap = n;
+        n +=2;
+        mesa[0][i].disp ="libre";
+        mesa[0][i].prec = prices[i];
+        
+        
+    }
+
+    n =2;
+    for(i = 0; i < 3; i++){
+        mesa[1][i].ubi = "afuera";
+        mesa[1][i].cap = n;
+        n +=2;
+        mesa[1][i].disp  = "libre";
+        mesa[1][i].prec = prices[i];
+        
+        
+    }
+    
+}
+
+
+void reserva(int ubi){
+    int n = num_personas();
+    int reserva = 0;
+    
+
+    for(int i = 0; i<3; i++){
+        if(mesa[ubi][i].cap == n && mesa[ubi][i].disp == "libre"){
+            mesa[ubi][i].disp = "ocupado";
+            reserva  = 1;
+            precio += mesa[ubi][i].prec;
+            
+        }
+        
+    }
+    if (reserva == 1){
+            printf("mesa reservada\n");
+            
+        }
+    else
+        printf("no se  en contro una mesa con esas caracteristicas \n ");
+        
+continuar();
+
+}
+
+
+void continuar(){
+    int opcion;
+    printf("Quiere seguir usando el programa? \n1.Si\n2.No\n");
+    scanf("%d", &opcion);
+
+    switch (opcion)
+    {
+    case 1:
+        menu();
+        
+
+    case 2:
+        
+        break;
+    
+    default:
+        printf("opcion invalida \n");
+        continuar();
+    }
 }
